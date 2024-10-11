3ec08b10d4c0150169f3249031ba70cd01bf4cd6
LOW <87609374+GhostLow@users.noreply.github.com>
2024-10-11 22:59:30 +0100

    new

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("mousemove", (event) => {
+    const background = document.querySelector(".background");
+    const moveX = (event.clientX / window.innerWidth - 0.5) * 20;
+    const moveY = (event.clientY / window.innerHeight - 0.5) * 20;
+    background.style.transform = `translate(${moveX}px, ${moveY}px)`;
+});
