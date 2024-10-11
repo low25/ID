b884be063690443023b3d4456e61686eac94d1ac
LOW <87609374+low25@users.noreply.github.com>
2024-10-11 23:30:17 +0100

    Changed

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ document.addEventListener("mousemove", (event) => {
     const background = document.querySelector(".background");
     const moveX = (event.clientX / window.innerWidth - 0.5) * 20;
     const moveY = (event.clientY / window.innerHeight - 0.5) * 20;
-    background.style.transform = `translate(${moveX}px, ${moveY}px)`;
+    background.style.transform = `translate(${moveX*1.5}px, ${moveY*1.5}px)`;
 });
