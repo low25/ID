079ca15a4623040c46c18949686e1965d5bcf2ff
LOW <87609374+low25@users.noreply.github.com>
2024-10-12 01:43:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,11 @@ document.addEventListener("mousemove", (event) => {
     background.style.transform = `translate(${moveX*1.5}px, ${moveY*1.5}px)`;
 });
 
+document.addEventListener("mousemove", (event) => {
+    const background2 = document.getElementById("background2");
+    const moveX = (event.clientX / window.innerWidth - 0.5) * 20;
+    const moveY = (event.clientY / window.innerHeight - 0.5) * 20;
+    background2.style.transform = `translate(${moveX*1.5}px, ${moveY*1.5}px)`;
+});
+
 
