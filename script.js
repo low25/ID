e6f5c323dc0d9555948a7ef150a7e9b294f04d73
LOW <87609374+low25@users.noreply.github.com>
2024-10-12 21:33:09 +0100

    bg move

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ document.addEventListener("mousemove", (event) => {
     const background = document.querySelector(".background");
     const moveX = (event.clientX / window.innerWidth - 0.5) * 20;
     const moveY = (event.clientY / window.innerHeight - 0.5) * 20;
-    background.style.transform = `translate(${moveX*1.5}px, ${moveY*1.5}px)`;
+    background.style.transform = `translate(${moveX*2}px, ${moveY*2}px)`;
 });
 
 document.addEventListener("mousemove", (event) => {
     const background2 = document.getElementById("background2");
     const moveX = (event.clientX / window.innerWidth - 0.5) * 20;
     const moveY = (event.clientY / window.innerHeight - 0.5) * 20;
-    background2.style.transform = `translate(${moveX*1.5}px, ${moveY*1.5}px)`;
+    background2.style.transform = `translate(${moveX*2}px, ${moveY*2}px)`;
 });
 
 const dotContainer = document.getElementById('dot-container');
